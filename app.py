c9c52fe08624e3ee0ccce1928f835a4a50af5531
RohanRamola <rohanramola63@gmail.com>
2023-08-22 03:50:36 +0530

    Created first route

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("hello world")
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def showProfile():
+    result = {
+        "name" : "rohan",
+        "status" : "ok"
+    }
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
