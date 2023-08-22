0109dcad3f9aaf0e0bf8af352140f1450043c341
RohanRamola <rohanramola63@gmail.com>
2023-08-23 04:55:02 +0530

    Added Post Method

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from mConnection import client
 import json
 
@@ -20,13 +20,40 @@ def showMeals():
     documents = collection.find()
     return json.dumps([meal for meal in documents], default=str)
 
-@app.route('/users', methods=['POST'])
+@app.route('/users', methods=['GET'])
 def showUsers():
     dbs = client.myApp
     collection = dbs.users
     documents = collection.find()
     return json.dumps([users for users in documents], default=str)
 
+@app.route('/user/new', methods=['POST'])
+def createUser():
+    content_type = request.headers.get('Content-Type')
+    print(content_type)
+    if(content_type == 'application/json'):
+        json = request.json
+        
+        dbs = client.myApp
+        collection = dbs.users
+
+        print(json['username'])
+        print(json['email'])
+        print(json['password'])
+        print(json['status'])
+
+        user = {
+            "username": json['username'],
+            "email": json['email'],
+            "password": json['password'],
+            "status": json['status']
+        }
+
+        collection.insert_one(user)
+
+        return json
+    else:
+        return 'Content-Type not supported'    
 
 if __name__ == "__main__":
     app.run(debug=True)
