8f3398d6a5ff3d0ba17bcae2ac1c1538b45e0c24
RohanRamola <rohanramola63@gmail.com>
2023-08-23 06:55:03 +0530

    Added all HTTP methods

--- app.py
+++ app.py
@@ -55,17 +55,60 @@ def createUser():
     else:
         return 'Content-Type not supported'    
 
-@app.route('/user/update/<string:username>', methods=['PUT'])
+@app.route('/user/update/<string:username>', methods=['POST', 'PUT'])
 def updateUser(username):
 
     dbs = client.myApp
     collection = dbs.users
 
     myQuery = {"username": username}
-    newValues = {"$set": {"status": 0, "password": "harry"}}
+    newValues = {"$set": {"status": 1, "password": "harry"}}
 
-    collection.update_many(myQuery, newValues)
-    return username +  "'s Profile updated successfully"
+    response = collection.update_many(myQuery, newValues)
+    if response.matched_count == 0:
+        return "No user found"
+    else:
+        return "Profile updated successfully"
+
+@app.route('/user/delete/<string:username>/<string:password>', methods=['DELETE'])
+def deleteUser(username, password):
+
+    dbs = client.myApp
+    collection = dbs.users
+
+    myQuery = {'username': username, 'password': password}
+    response = collection.delete_many(myQuery)
+
+    if response.deleted_count == 0:
+        return "No data matched", 404
+    else:
+        return "User deleted successfully"
+
+@app.route('/user/data/<string:email>/<string:newEmail>', methods=['PATCH'])
+def updateData(email, newEmail):
+
+    dbs = client.myApp
+    collection = dbs.users
+
+    myQuery = {'email': email}
+    newValues = {'$set': {'email': newEmail}}
+
+    response = collection.update_one(myQuery, newValues)
+    if response.matched_count == 0:
+        return 'No data found', 404
+    else:
+        return 'Email updated successfully'
+
+@app.route('/api/headers', methods=['HEAD'])
+def showHeaders():
+    headers = {'Content-Type': 'application/json'}
+    return 'Check headers', 200, headers
+
+@app.route('/users/methods', methods=['OPTIONS'])
+def callOptions():
+
+    options = {'Alow': 'GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS'}
+    return 'Check Headers', 200, options
 
 if __name__ == "__main__":
     app.run(debug=True)
